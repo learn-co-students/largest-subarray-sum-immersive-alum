c1cdc5df5dcbcebec9417b69738bffaff8856c5f
github username <github email address>
2017-10-22 17:03:30 -0400

    solved problem

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
 
 
-function largestSubArraySum(array){
+function largestSubarraySum(array){
   let max = array[0]
   let currentMax = array[0]
 
-  array.forEach(function(value){
-    currentMax += value
+  for(let i = 1; i < array.length; i++){
+    currentMax += array[i]
     if(max < currentMax){
       max = currentMax
     }
     if(currentMax < 0){
       currentMax = 0
     }
-  })
+  }
   return max
 }
